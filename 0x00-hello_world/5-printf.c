d56bcca9f2de00fd31761e445cbd22ce578efcaa
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-09-09 13:55:26 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This is the main function  
+ * Main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
