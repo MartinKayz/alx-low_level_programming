9c14566b385ba749ca88ad901e74ceeb6555b779
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-09-09 13:33:27 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-/**This is a C- program that describes 
- *How the analogy of Programming 
- *is
- */
 #include <stdio.h>
+
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success )
  */
 int main(void)
 {
-	/*This is the function main*/
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
