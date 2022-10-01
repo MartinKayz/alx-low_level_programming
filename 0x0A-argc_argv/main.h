bb85e6d3431a42991bd038d08c07ba0e19eb13a7
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-09-30 18:34:16 -0700

    Best argument

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,7 @@
 void _putchar(char c);
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
 
 
 
