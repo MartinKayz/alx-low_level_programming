c2256f320e7f376162aa3b356b65a31d6b19cba5
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-10-05 06:43:25 -0700

    Unix, really..

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,14 +7,14 @@
 
 #include<stdio.h>
 /**
- * main() - Entry Point
+ * main - Entry Point
  *
  * Return: Always return 1 (Success)
  *
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
