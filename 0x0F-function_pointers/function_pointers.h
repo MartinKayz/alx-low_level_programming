e2945e6ad8840022712b3bb5a8f3f28a5017bda6
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-10-12 07:24:28 -0700

    what is my name again

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
-# ifndef function_pointer_h
-# define function_pointers_h
-
+# ifndef _function_pointer_h
+# define _function_pointers_h
+# include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
-# endif
+#endif
