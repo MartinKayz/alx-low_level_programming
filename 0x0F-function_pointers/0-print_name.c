9f3c219ed935b9979bb4fa498eea953c0b40f9f3
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-10-12 06:42:20 -0700

    Modified, what is my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
 # include "function_pointers.h"
+/**
+* print_name - Returns something
+* description: This is a desc
+* @name: The name to be printed
+* @f: function pointer
+*
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
